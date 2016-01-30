39ad0e1fff805816e5da475c90273cf27d34696e
ohrely <rebaely@gmail.com>
2016-01-30 14:32:11 -0800

    Further efforts to save to storage hindered by console logging trickiness.  Will resume pair programming remotely later.

--- popup.js
+++ popup.js
@@ -8,16 +8,22 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
+function submitted() {
+  var find = document.getElementsByName("find");
+  document.getElementById("status").textContent = "HEEEEEEUU";
+  var replace = document.getElementsByName("replace");
+  console.log(replace);
 
-
-
+  addWord(find, replace);
+};
 
 function addWord(find, replace) {
+  // TODO: Anticipate problems with overwriting save, will change later.
   var save = {};
   save[find] = replace;
 
   chrome.storage.sync.set(save, function() {
-      console.log('Settings saved');
+      console.log(save);
   });
 };
 
@@ -61,46 +67,46 @@ function addWord(find, replace) {
 //   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 // }
 
-// /**
-//  * @param {string} searchTerm - Search term for Google Image search.
-//  * @param {function(string,number,number)} callback - Called when an image has
-//  *   been found. The callback gets the URL, width and height of the image.
-//  * @param {function(string)} errorCallback - Called when the image is not found.
-//  *   The callback gets a string that describes the failure reason.
-//  */
-// function getImageUrl(searchTerm, callback, errorCallback) {
-//   // Google image search - 100 searches per day.
-//   // https://developers.google.com/image-search/
-//   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-//     '?v=1.0&q=' + encodeURIComponent(searchTerm);
-//   var x = new XMLHttpRequest();
-//   x.open('GET', searchUrl);
-//   // The Google image search API responds with JSON, so let Chrome parse it.
-//   x.responseType = 'json';
-//   x.onload = function() {
-//     // Parse and process the response from Google Image Search.
-//     var response = x.response;
-//     if (!response || !response.responseData || !response.responseData.results ||
-//         response.responseData.results.length === 0) {
-//       errorCallback('No response from Google Image search!');
-//       return;
-//     }
-//     var firstResult = response.responseData.results[0];
-//     // Take the thumbnail instead of the full image to get an approximately
-//     // consistent image size.
-//     var imageUrl = firstResult.tbUrl;
-//     var width = parseInt(firstResult.tbWidth);
-//     var height = parseInt(firstResult.tbHeight);
-//     console.assert(
-//         typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-//         'Unexpected respose from the Google Image Search API!');
-//     callback(imageUrl, width, height);
-//   };
-//   x.onerror = function() {
-//     errorCallback('Network error.');
-//   };
-//   x.send();
-// }
+// // *
+// //  * @param {string} searchTerm - Search term for Google Image search.
+// //  * @param {function(string,number,number)} callback - Called when an image has
+// //  *   been found. The callback gets the URL, width and height of the image.
+// //  * @param {function(string)} errorCallback - Called when the image is not found.
+// //  *   The callback gets a string that describes the failure reason.
+ 
+// // function getImageUrl(searchTerm, callback, errorCallback) {
+// //   // Google image search - 100 searches per day.
+// //   // https://developers.google.com/image-search/
+// //   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
+// //     '?v=1.0&q=' + encodeURIComponent(searchTerm);
+// //   var x = new XMLHttpRequest();
+// //   x.open('GET', searchUrl);
+// //   // The Google image search API responds with JSON, so let Chrome parse it.
+// //   x.responseType = 'json';
+// //   x.onload = function() {
+// //     // Parse and process the response from Google Image Search.
+// //     var response = x.response;
+// //     if (!response || !response.responseData || !response.responseData.results ||
+// //         response.responseData.results.length === 0) {
+// //       errorCallback('No response from Google Image search!');
+// //       return;
+// //     }
+// //     var firstResult = response.responseData.results[0];
+// //     // Take the thumbnail instead of the full image to get an approximately
+// //     // consistent image size.
+// //     var imageUrl = firstResult.tbUrl;
+// //     var width = parseInt(firstResult.tbWidth);
+// //     var height = parseInt(firstResult.tbHeight);
+// //     console.assert(
+// //         typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
+// //         'Unexpected respose from the Google Image Search API!');
+// //     callback(imageUrl, width, height);
+// //   };
+// //   x.onerror = function() {
+// //     errorCallback('Network error.');
+// //   };
+// //   x.send();
+// // }
 
 // function renderStatus(statusText) {
 //   document.getElementById('status').textContent = statusText;
